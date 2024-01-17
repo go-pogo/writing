1028597fee554ec113d94253bd9bd3a796b26cd9
roeldev <roeldev@users.noreply.github.com>
2024-01-17 19:18:56 +0100

    Make BytesBufferPool zero value ready to use

--- bytes.go
+++ bytes.go
@@ -10,32 +10,41 @@ import (
 )
 
 // A BytesBufferPool is a sync.Pool which stores bytes.Buffer(s).
+// Its zero value is ready for use.
 type BytesBufferPool struct {
-	p sync.Pool
+	pool sync.Pool
+	once sync.Once
 }
 
 // NewBytesBufferPool creates a new BytesBufferPool with a given initial
 // capacity n for each retrieved bytes.Buffer.
 func NewBytesBufferPool(n int) *BytesBufferPool {
-	return &BytesBufferPool{p: sync.Pool{
-		New: func() interface{} {
+	var p BytesBufferPool
+	p.init(n)
+	return &p
+}
+
+func (p *BytesBufferPool) init(n int) {
+	p.once.Do(func() {
+		p.pool.New = func() interface{} {
 			var buf bytes.Buffer
 			if n != 0 {
 				buf.Grow(n)
 			}
 			return &buf
-		},
-	}}
+		}
+	})
 }
 
 // Get selects an arbitrary bytes.Buffer from the BytesBufferPool, removes it
 // from the pool, and returns it to the caller.
 func (p *BytesBufferPool) Get() *bytes.Buffer {
-	return p.p.Get().(*bytes.Buffer)
+	p.init(0)
+	return p.pool.Get().(*bytes.Buffer)
 }
 
 // Put adds the bytes.Buffer b to the pool.
 func (p *BytesBufferPool) Put(b *bytes.Buffer) {
 	b.Reset()
-	p.p.Put(b)
+	p.pool.Put(b)
 }
